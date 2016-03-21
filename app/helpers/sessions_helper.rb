7ecad084e55875a19aab91a93d7c54e9e37767cd
Alex Takacs <alexander.g.takacs@gmail.com>
2016-03-21 13:22:47 -0500

    assignment_38_rails_authentication_signing_in:completed

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,4 +1,5 @@
 module SessionsHelper
+
   def create_session(user)
     session[:user_id] = user.id
   end
@@ -7,8 +8,13 @@ module SessionsHelper
     session[:user_id] = nil
   end
 
-
   def current_user
     User.find_by(id: session[:user_id])
   end
+
+  def avatar_url(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+     "https://s.gravatar.com/avatar/#{gravatar_id}?s=80"
+  end
+
 end
